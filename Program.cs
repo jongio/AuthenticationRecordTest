5036ab28e56a416720dc90a411c8533d77744bc9
Jon Gallant <jon@jongallant.com>
2021-01-29 17:26:20 -0800

    Update with using blocks and more examples

--- Program.cs
+++ Program.cs
@@ -14,36 +14,58 @@ namespace AuthenticationRecordTest
             var credentialOne = new InteractiveBrowserCredential(
                 new InteractiveBrowserCredentialOptions
                 {
-                    TokenCache = new PersistentTokenCache()
+                    TokenCache = new PersistentTokenCache(new PersistentTokenCacheOptions() { Name = "AuthenticationRecord.cache" })
                 });
 
             // 2. Prompt user to authenticate
             AuthenticationRecord authRecordWrite = await credentialOne.AuthenticateAsync();
 
             // 3. Save AuthenticationRecord to disk
-            using var authRecordStreamWrite = new FileStream("AuthRecord.json", FileMode.Create, FileAccess.Write);
-            await authRecordWrite.SerializeAsync(authRecordStreamWrite);
-            await authRecordStreamWrite.FlushAsync();
+            using (var authRecordStreamWrite = new FileStream("AuthRecord.json", FileMode.Create, FileAccess.Write))
+            {
+                await authRecordWrite.SerializeAsync(authRecordStreamWrite);
+            }
 
             // A future user session where we want to silent auth with TokenCache and AuthenticationRecord
 
-            // 4. Read the AutheticationRecord from disk
-            using var authRecordStreamRead = new FileStream("AuthRecord.json", FileMode.Open, FileAccess.Read);
-            AuthenticationRecord authRecordRead = await AuthenticationRecord.DeserializeAsync(authRecordStreamRead);
+            // 4. Read the AuthenticationRecord from disk
+            AuthenticationRecord authRecordRead;
+            using (var authRecordStreamRead = new FileStream("AuthRecord.json", FileMode.Open, FileAccess.Read))
+            {
+                authRecordRead = await AuthenticationRecord.DeserializeAsync(authRecordStreamRead);
+            }
 
             // 5. Create TokenCredential object with TokenCache and use persisted AuthenticationRecord
             var credentialTwo = new InteractiveBrowserCredential(
                 new InteractiveBrowserCredentialOptions
                 {
-                    TokenCache = new PersistentTokenCache(),
+                    TokenCache = new PersistentTokenCache(new PersistentTokenCacheOptions() { Name = "AuthenticationRecord.cache" }),
                     AuthenticationRecord = authRecordRead
                 });
 
+            // 5.1 Same as above but with DisableAutomaticAuthentication set to true
+            // var credentialTwo = new InteractiveBrowserCredential(
+            //     new InteractiveBrowserCredentialOptions
+            //     {
+            //         TokenCache = new PersistentTokenCache(new PersistentTokenCacheOptions() { Name = "AuthenticationRecord.cache" }),
+            //         AuthenticationRecord = authRecordRead,
+            //         DisableAutomaticAuthentication = true
+            //     });
+
             // 6. Use the new TokenCredential object. User will not be prompted to re-authenticate if token has expired.
             var client = new SecretClient(new Uri("https://memealyzerdev9kv.vault.azure.net/"), credentialTwo);
-            var secret = await client.GetSecretAsync("CosmosKey");
 
-            Console.WriteLine(secret.Value.Value);
+            try
+            {
+                var secret = await client.GetSecretAsync("CosmosKey");
+                Console.WriteLine(secret.Value.Value);
+
+            }
+            catch (AuthenticationRequiredException ex)
+            {
+                Console.WriteLine("You set InteractiveBrowserCredentialOptions.DisableAutomaticAuthentication to true and the user token has expired or has been revoked.");
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
